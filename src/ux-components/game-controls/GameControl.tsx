a714757dc523f67c3ec99f431db98e46835c6034
eugene.karpenko <eugene.karpenko@jetbrains.com>
2025-09-04 19:46:52 +0200

    Replace buttons with image-based controls in GameControl.

--- src/ux-components/game-controls/GameControl.tsx
+++ src/ux-components/game-controls/GameControl.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from './css/GameControl.module.css';
+import NewGame from '../../assets/images/NewGame.png';
+import LoadGame from '../../assets/images/LoadGame.png';
+import SaveGame from '../../assets/images/SaveGame.png';
 
 interface GameControlProps {
   onNewGame?: () => void;
@@ -30,15 +33,24 @@ const GameControl: React.FC<GameControlProps> = ({ onNewGame, onLoadGame, onSave
 
   return (
     <div className={styles.gameControlContainer}>
-      <button className={styles.gameControlButton} onClick={handleNewGame}>
-        New Game
-      </button>
-      <button className={styles.gameControlButton} onClick={handleLoadGame}>
-        Load Game
-      </button>
-      <button className={styles.gameControlButton} onClick={handleSaveGame}>
-        Save Game
-      </button>
+      <img
+        src={NewGame}
+        alt="New Game"
+        className={styles.gameControlButtonImage}
+        onClick={handleNewGame}
+      />
+      <img
+        src={LoadGame}
+        alt="Load Game"
+        className={styles.gameControlButtonImage}
+        onClick={handleLoadGame}
+      />
+      <img
+        src={SaveGame}
+        alt="Save Game"
+        className={styles.gameControlButtonImage}
+        onClick={handleSaveGame}
+      />
     </div>
   );
 };
