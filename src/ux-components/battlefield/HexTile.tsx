d6f3daff3c21884ee5b4756d214e53cb4378fc59
eugene.karpenko <eugene.karpenko@jetbrains.com>
2025-09-22 17:29:55 +0200

    Add background color for controled Lands and remove background from Land Images to make background visible

--- src/ux-components/battlefield/HexTile.tsx
+++ src/ux-components/battlefield/HexTile.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import styles from './css/Hexagonal.module.css';
-import { createTileId, GameState } from '../../types/HexTileState';
+import { createTileId, GameState, getPlayerById } from '../../types/HexTileState';
 import LandCharacteristicsPopup from './LandCharacteristicsPopup';
 import { Position } from '../../map/utils/mapTypes';
+import { PLAYER_COLORS } from '../../types/PlayerColors';
 
 import darkforestImg from '../../assets/map-tiles/darkforest.png';
 import greenforestImg from '../../assets/map-tiles/greenforest.png';
@@ -43,6 +44,19 @@ const HexTile: React.FC<HexTileProps> = ({ battlefieldPosition, gameState }) => 
   const imageSrc = imageMap[battlefieldTile.landType.imageName];
   const altText = battlefieldTile.landType.name;
 
+  // Get the controlling player's color or default to white if not controlled
+  const getBackgroundColor = (): string => {
+    if (!battlefieldTile.controlledBy) {
+      return 'white';
+    }
+    const controllingPlayer = getPlayerById(gameState, battlefieldTile.controlledBy);
+    if (!controllingPlayer) {
+      return 'white';
+    }
+    const playerColor = PLAYER_COLORS.find((color) => color.name === controllingPlayer.color);
+    return playerColor?.value ?? 'white';
+  };
+
   const handleRightClick = (event: React.MouseEvent) => {
     event.preventDefault();
     setPopupPosition({ x: event.clientX, y: event.clientY });
@@ -59,6 +73,7 @@ const HexTile: React.FC<HexTileProps> = ({ battlefieldPosition, gameState }) => 
         className={styles.hexTile}
         title={`${battlefieldTile.landType.name} (${battlefieldTile.landType.alignment})`}
         onContextMenu={handleRightClick}
+        style={{ backgroundColor: getBackgroundColor() }}
       >
         {imageSrc ? (
           <img src={imageSrc} alt={altText} className={styles.hexTileImg} />
