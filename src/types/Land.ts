7a2ecdeacee05d46580efe883d631aee4dbe84fc
eugene.karpenko <eugene.karpenko@jetbrains.com>
2025-10-12 12:31:39 +0200

    Decrease gold production to void a lot of gold in the game

--- src/types/Land.ts
+++ src/types/Land.ts
@@ -27,55 +27,55 @@ export const getLandById = (id: LAND_TYPE): Land => {
       return {
         id: LAND_TYPE.PLAINS,
         alignment: Alignment.NEUTRAL,
-        goldPerTurn: { min: 2000, max: 3000 },
+        goldPerTurn: { min: 650, max: 1000 },
       };
     case LAND_TYPE.MOUNTAINS:
       return {
         id: LAND_TYPE.MOUNTAINS,
         alignment: Alignment.LAWFUL,
-        goldPerTurn: { min: 2700, max: 3400 },
+        goldPerTurn: { min: 900, max: 1150 },
       };
     case LAND_TYPE.GREEN_FOREST:
       return {
         id: LAND_TYPE.GREEN_FOREST,
         alignment: Alignment.LAWFUL,
-        goldPerTurn: { min: 2500, max: 2800 },
+        goldPerTurn: { min: 800, max: 950 },
       };
     case LAND_TYPE.DARK_FOREST:
       return {
         id: LAND_TYPE.DARK_FOREST,
         alignment: Alignment.CHAOTIC,
-        goldPerTurn: { min: 2500, max: 2800 },
+        goldPerTurn: { min: 800, max: 950 },
       };
     case LAND_TYPE.HILLS:
       return {
         id: LAND_TYPE.HILLS,
         alignment: Alignment.NEUTRAL,
-        goldPerTurn: { min: 1500, max: 2200 },
+        goldPerTurn: { min: 500, max: 700 },
       };
     case LAND_TYPE.SWAMP:
       return {
         id: LAND_TYPE.SWAMP,
         alignment: Alignment.CHAOTIC,
-        goldPerTurn: { min: 1100, max: 1700 },
+        goldPerTurn: { min: 350, max: 550 },
       };
     case LAND_TYPE.DESERT:
       return {
         id: LAND_TYPE.DESERT,
         alignment: Alignment.NEUTRAL,
-        goldPerTurn: { min: 500, max: 700 },
+        goldPerTurn: { min: 150, max: 270 },
       };
     case LAND_TYPE.LAVA:
       return {
         id: LAND_TYPE.LAVA,
         alignment: Alignment.CHAOTIC,
-        goldPerTurn: { min: 1500, max: 1900 },
+        goldPerTurn: { min: 500, max: 600 },
       };
     case LAND_TYPE.VOLCANO:
       return {
         id: LAND_TYPE.VOLCANO,
         alignment: Alignment.CHAOTIC,
-        goldPerTurn: { min: 3000, max: 3000 },
+        goldPerTurn: { min: 1000, max: 1000 },
       };
     default:
       // use on map generation
