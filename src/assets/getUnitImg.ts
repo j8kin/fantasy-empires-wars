d99e3c6ab837166e28849662d484b2db488b3264
eugene.karpenko <eugene.karpenko@jetbrains.com>
2025-11-06 08:29:27 +0100

    Add images for regular units: warrior, dwarf, elf, dark elf and orc to display in recruit dialog

--- src/assets/getUnitImg.ts
+++ src/assets/getUnitImg.ts
@@ -1,5 +1,24 @@
-import { UnitType } from '../types/Army';
+import { RegularUnitType, UnitType } from '../types/Army';
+
+import warriorImg from './army/warrior.png';
+import dwarfImg from './army/dwarf.png';
+import elfImg from './army/elf.png';
+import darkElfImg from './army/dark-elf.png';
+import orcImg from './army/orc.png';
 
 export const getUnitImg = (unit: UnitType) => {
-  return undefined;
+  switch (unit) {
+    case RegularUnitType.WARRIOR:
+      return warriorImg;
+    case RegularUnitType.DWARF:
+      return dwarfImg;
+    case RegularUnitType.ELF:
+      return elfImg;
+    case RegularUnitType.DARK_ELF:
+      return darkElfImg;
+    case RegularUnitType.ORC:
+      return orcImg;
+    default:
+      return undefined;
+  }
 };
