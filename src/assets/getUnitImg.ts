ba35f0e73d41f1498eb248ced9264d1076b6bf4b
eugene.karpenko <eugene.karpenko@jetbrains.com>
2025-11-07 15:28:24 +0100

    Add images for fighter, hummerlord and ranger hero units which are used in the recruit dialog

--- src/assets/getUnitImg.ts
+++ src/assets/getUnitImg.ts
@@ -1,4 +1,4 @@
-import { RegularUnitType, UnitType } from '../types/Army';
+import { HeroUnitType, RegularUnitType, UnitType } from '../types/Army';
 
 import warriorImg from './army/warrior.png';
 import dwarfImg from './army/dwarf.png';
@@ -7,6 +7,10 @@ import darkElfImg from './army/dark-elf.png';
 import orcImg from './army/orc.png';
 import ballistaImg from './army/ballista.png';
 import catapultImg from './army/catapult.png';
+
+import fightableImg from './army/fighter.png';
+import hummerLordImg from './army/hummerlord.png';
+import rangerImg from './army/ranger.png';
 
 export const getUnitImg = (unit: UnitType) => {
   switch (unit) {
@@ -24,6 +28,14 @@ export const getUnitImg = (unit: UnitType) => {
       return ballistaImg;
     case RegularUnitType.CATAPULT:
       return catapultImg;
+
+    case HeroUnitType.FIGHTER:
+      return fightableImg;
+    case HeroUnitType.HAMMER_LORD:
+      return hummerLordImg;
+    case HeroUnitType.RANGER:
+      return rangerImg;
+
     default:
       return undefined;
   }
