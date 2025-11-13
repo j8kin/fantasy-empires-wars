409b39e842dbeb0d1fdb47e91a19b8ea5c123c5f
eugene.karpenko <eugene.karpenko@jetbrains.com>
2025-11-13 17:59:35 +0100

    Add Quest images

--- src/assets/getQuestImg.ts
+++ src/assets/getQuestImg.ts
@@ -1,5 +1,14 @@
 import { QuestType } from '../types/Quest';
 
+import theEchoingRuinsImg from './quest/the-echoing-ruins.png';
+import theWhisperingGroveImg from './quest/the-whispering-grove.png';
+
 export const getQuestImg = (questType: QuestType): string | undefined => {
+  switch (questType) {
+    case 'The Echoing Ruins':
+      return theEchoingRuinsImg;
+    case 'The Whispering Grove':
+      return theWhisperingGroveImg;
+  }
   return undefined;
 };
