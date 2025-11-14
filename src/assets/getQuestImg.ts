331c2e979456c977f3cf3f6c52ca29086d0f2c6e
eugene.karpenko <eugene.karpenko@jetbrains.com>
2025-11-14 09:13:21 +0100

    Add quest images

--- src/assets/getQuestImg.ts
+++ src/assets/getQuestImg.ts
@@ -2,13 +2,18 @@ import { QuestType } from '../types/Quest';
 
 import theEchoingRuinsImg from './quest/the-echoing-ruins.png';
 import theWhisperingGroveImg from './quest/the-whispering-grove.png';
+import theAbyssalCryptImg from './quest/the-abyssal-crypt.png';
+import theShatteredSkyImg from './quest/the-shattered-sky.png';
 
-export const getQuestImg = (questType: QuestType): string | undefined => {
+export const getQuestImg = (questType: QuestType): string => {
   switch (questType) {
     case 'The Echoing Ruins':
       return theEchoingRuinsImg;
     case 'The Whispering Grove':
       return theWhisperingGroveImg;
+    case 'The Abyssal Crypt':
+      return theAbyssalCryptImg;
+    case 'The Shattered Sky':
+      return theShatteredSkyImg;
   }
-  return undefined;
 };
