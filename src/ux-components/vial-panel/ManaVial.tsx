2585c1b5fd1d686fc471352c95e9c2e84c422186
eugene.karpenko <eugene.karpenko@jetbrains.com>
2025-11-18 14:30:31 +0100

    Initial draft of mana vial

--- src/ux-components/vial-panel/ManaVial.tsx
+++ src/ux-components/vial-panel/ManaVial.tsx
@@ -1,28 +1,37 @@
 import React from 'react';
 import styles from './css/ManaVial.module.css';
 
-interface ManaVialProps {
-  color: string;
+import { getManaColor, getManaGradient, ManaType, MAX_MANA } from '../../types/Mana';
+
+import { getManaVialImg } from '../../assets/getManaVialImg';
+
+interface FilledManaVialProps {
+  color: ManaType;
   mana?: number;
 }
 
-const ManaVial: React.FC<ManaVialProps> = ({ color, mana }) => {
+const ManaVial: React.FC<FilledManaVialProps> = ({ color, mana }) => {
   if (mana == null) return null;
 
-  const MANA_100_PERCENTAGE = 200;
-  const percentage = mana >= MANA_100_PERCENTAGE ? 100 : mana / 2;
+  const percentage = mana >= MAX_MANA ? 100 : (mana * 100) / MAX_MANA;
+  const opacity = percentage / 30;
 
+  const [baseColor, darkerColor] = getManaGradient(color);
   const fillStyle: React.CSSProperties = {
-    height: `${percentage}%`,
-    backgroundColor: color,
+    background: `linear-gradient(${baseColor}, ${darkerColor})`,
+    boxShadow: `0 0 8px ${getManaColor(color)}80`,
+    opacity: opacity,
   };
 
   return (
-    <div className={styles.ball} data-testid={color + '-mana-vial'}>
-      <div className={styles.fill} style={fillStyle}></div>
-      <span className={styles.value} data-testid={color + '-mana-vial-percentage'}>
-        {percentage}%
-      </span>
+    <div className={styles.vialContainer} data-testid={color + '-filled-mana-vial'}>
+      <div className={styles.fillContainer}>
+        <div className={styles.fill} style={fillStyle}></div>
+      </div>
+      <img src={getManaVialImg()} className={styles.vialImage} alt={`${color} mana vial`} />
+      {/*<span className={styles.value} data-testid={color + '-mana-vial-percentage'}>*/}
+      {/*  {percentage}%*/}
+      {/*</span>*/}
     </div>
   );
 };
