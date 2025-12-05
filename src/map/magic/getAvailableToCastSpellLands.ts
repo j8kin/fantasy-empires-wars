93b715f7faff62d5019d8a6e0d829daa9d30a8e5
eugene.karpenko <eugene.karpenko@jetbrains.com>
2025-12-05 18:39:50 +0100

    Refactor `getAvailableToCastSpellLands`: Improve affected land filtering logic to exclude both army-affected and land-affected tiles for spells. Remove redundant import.

--- src/map/magic/getAvailableToCastSpellLands.ts
+++ src/map/magic/getAvailableToCastSpellLands.ts
@@ -2,10 +2,10 @@ import { GameState } from '../../state/GameState';
 import { getLandId } from '../../state/map/land/LandId';
 
 import { getPlayerLands, getTurnOwner } from '../../selectors/playerSelectors';
+import { getArmiesByPlayer, getPosition } from '../../selectors/armySelectors';
 import { getSpellById } from '../../selectors/spellSelectors';
 
 import { SpellName } from '../../types/Spell';
-import { getArmiesByPlayer, getPosition } from '../../selectors/armySelectors';
 
 export const getAvailableToCastSpellLands = (gameState: GameState, spellName: SpellName) => {
   const spell = getSpellById(spellName);
@@ -20,15 +20,21 @@ export const getAvailableToCastSpellLands = (gameState: GameState, spellName: Sp
     .flatMap((player) => {
       // exclude all players lands if player already effected by this spell
       if (player.effects.some((effect) => effect.spell === spellName)) return [];
-      // exclude all lands with effects from this spell
-      const landsWithArmiesAffectedByThisSpell = getArmiesByPlayer(gameState, player.id)
+
+      // exclude all lands with armies which already have effects from this spell
+      const affectedArmiesLands = getArmiesByPlayer(gameState, player.id)
         .filter((a) => a.effects.some((e) => e.spell === spellName))
         .map((a) => getLandId(getPosition(a)));
 
-      console.log(`getAvailableToCastSpellLands: ${landsWithArmiesAffectedByThisSpell}`);
+      // exclude all lands with effects from this spell
+      const affectedLands = getPlayerLands(gameState, player.id)
+        .filter((l) => l.effects.some((e) => e.spell === spellName))
+        .map((l) => getLandId(l.mapPos));
+
       return getPlayerLands(gameState, player.id).filter(
         (l) =>
-          !landsWithArmiesAffectedByThisSpell.includes(getLandId(l.mapPos)) &&
+          !affectedArmiesLands.includes(getLandId(l.mapPos)) &&
+          !affectedLands.includes(getLandId(l.mapPos)) &&
           l.effects.every((e) => e.spell !== spellName)
       );
     })
