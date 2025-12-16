835ac04356ef6ba8f3fe78d2c3041004387d361c
eugene.karpenko <eugene.karpenko@jetbrains.com>
2025-12-16 16:06:14 +0100

    Add new treasures, and update `getTreasureImg` logic

--- src/assets/getTreasureImg.ts
+++ src/assets/getTreasureImg.ts
@@ -1,10 +1,13 @@
-import { TreasureType } from '../types/Treasures';
-import { EmpireTreasure } from '../types/Treasures';
+import { EmpireTreasure, TreasureType } from '../types/Treasures';
 
-import mirrorOfIllusionImg from './treasures/mirror-of-illusion.png';
 import orbOfStormImg from './treasures/orb-of-storm.png';
 import wandOfTurnUndeadImg from './treasures/turn-undead.png';
 import phoenixFeatherImg from './treasures/phoenix-feather.png';
+
+import mirrorOfIllusionImg from './treasures/mirror-of-illusion.png';
+import bannerOfUnityImg from './treasures/banner-of-unity.png';
+import crownOfDominionImg from './treasures/crown-of-dominion.png';
+import shardOfTheSilentAnvil from './treasures/shard-of-silent-avil.png'
 
 export const getTreasureImg = (treasure: EmpireTreasure) => {
   switch (treasure.treasure.type) {
@@ -16,6 +19,12 @@ export const getTreasureImg = (treasure: EmpireTreasure) => {
       return phoenixFeatherImg;
     case TreasureType.MIRROR_OF_ILLUSION:
       return mirrorOfIllusionImg;
+    case TreasureType.BANNER_OF_UNITY:
+      return bannerOfUnityImg;
+    case TreasureType.CROWN_OF_DOMINION:
+      return crownOfDominionImg;
+    case TreasureType.SHARD_OF_THE_SILENT_ANVIL:
+      return shardOfTheSilentAnvil;
   }
   return undefined;
 };
