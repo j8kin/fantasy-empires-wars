c63a23d09d434f61c0f1cc623efae9b38d32ad95
eugene.karpenko <eugene.karpenko@jetbrains.com>
2025-12-18 15:24:12 +0100

    Add new treasures and images to `getTreasureImg` function.

--- src/assets/getTreasureImg.ts
+++ src/assets/getTreasureImg.ts
@@ -1,5 +1,4 @@
-import { TreasureType } from '../types/Treasures';
-import type { EmpireTreasure } from '../types/Treasures';
+import { EmpireTreasure, TreasureType } from '../types/Treasures';
 
 import orbOfStormImg from './treasures/orb-of-storm.png';
 import wandOfTurnUndeadImg from './treasures/turn-undead.png';
@@ -8,7 +7,12 @@ import phoenixFeatherImg from './treasures/phoenix-feather.png';
 import mirrorOfIllusionImg from './treasures/mirror-of-illusion.png';
 import bannerOfUnityImg from './treasures/banner-of-unity.png';
 import crownOfDominionImg from './treasures/crown-of-dominion.png';
+import scepterOfTempestsImg from './treasures/scepter-of-tempests.png';
 import shardOfTheSilentAnvil from './treasures/shard-of-silent-avil.png';
+import verdantIdolImg from './treasures/verdant-idol.png';
+import heartstoneOfOrrivaneImg from './treasures/heartstone-of-orrivane.png';
+import obsidianChaliceImg from './treasures/obsidian-chalice.png';
+import starwellPrismImg from './treasures/starwell-prism.png';
 
 export const getTreasureImg = (treasure: EmpireTreasure) => {
   switch (treasure.treasure.type) {
@@ -24,8 +28,18 @@ export const getTreasureImg = (treasure: EmpireTreasure) => {
       return bannerOfUnityImg;
     case TreasureType.CROWN_OF_DOMINION:
       return crownOfDominionImg;
+    case TreasureType.SCEPTER_OF_TEMPESTS:
+      return scepterOfTempestsImg;
     case TreasureType.SHARD_OF_THE_SILENT_ANVIL:
       return shardOfTheSilentAnvil;
+    case TreasureType.HEARTSTONE_OF_ORRIVANE:
+      return heartstoneOfOrrivaneImg;
+    case TreasureType.VERDANT_IDOL:
+      return verdantIdolImg;
+    case TreasureType.OBSIDIAN_CHALICE:
+      return obsidianChaliceImg;
+    case TreasureType.STARWELL_PRISM:
+      return starwellPrismImg;
   }
   return undefined;
 };
