206bca15843075d5420cf36f4ff3a76a96cce00f
eugene.karpenko <eugene.karpenko@jetbrains.com>
2026-01-07 11:17:27 +0100

    Refactor `landRelationships` to replace switch-case logic with lookup maps; improve readability in `placeHomeland` test.

--- src/domain/land/landRelationships.ts
+++ src/domain/land/landRelationships.ts
@@ -1,48 +1,36 @@
 import { LandName } from '../../types/Land';
 import type { LandType } from '../../types/Land';
 
+const surroundingLands: Partial<Record<LandType, LandType[]>> = {
+  [LandName.SUN_SPIRE_PEAKS]: [LandName.DARK_FOREST, LandName.HILLS],
+  [LandName.HEARTWOOD_GROVE]: [LandName.SWAMP, LandName.GREEN_FOREST],
+  [LandName.CRISTAL_BASIN]: [LandName.DESERT, LandName.HILLS],
+  [LandName.VOLCANO]: [LandName.MOUNTAINS, LandName.DARK_FOREST],
+  [LandName.SHADOW_MIRE]: [LandName.PLAINS, LandName.SWAMP],
+};
 /**
  * Returns the land types that surround a special land type
  * @returns Array of land types that can surround the given special land
  * @param landKind
  */
 export const getSurroundingLands = (landKind: LandType): LandType[] => {
-  switch (landKind) {
-    case LandName.VOLCANO:
-      return [LandName.MOUNTAINS, LandName.DARK_FOREST];
-    case LandName.SUN_SPIRE_PEAKS:
-      return [LandName.DARK_FOREST, LandName.HILLS];
-    case LandName.HEARTWOOD_GROVE:
-      return [LandName.SWAMP, LandName.GREEN_FOREST];
-    case LandName.CRISTAL_BASIN:
-      return [LandName.DESERT, LandName.HILLS];
-    case LandName.SHADOW_MIRE:
-      return [LandName.PLAINS, LandName.SWAMP];
-    default:
-      return [];
-  }
+  return surroundingLands[landKind] ?? [];
 };
 
+const nearbySpecialLands: Partial<Record<LandType, LandType>> = {
+  [LandName.SUN_SPIRE_PEAKS]: LandName.GOLDEN_PLAINS,
+  [LandName.HEARTWOOD_GROVE]: LandName.VERDANT_GLADE,
+  [LandName.CRISTAL_BASIN]: LandName.MISTY_GLADES,
+  [LandName.VOLCANO]: LandName.LAVA,
+  [LandName.SHADOW_MIRE]: LandName.BLIGHTED_FEN,
+};
 /**
  * Maps main special lands to their nearby counterpart lands
  * @param id - The main special land type
  * @returns The corresponding nearby special land type
  */
 export const getNearSpecialLandKinds = (id: LandType): LandType => {
-  switch (id) {
-    case LandName.VOLCANO:
-      return LandName.LAVA;
-    case LandName.SUN_SPIRE_PEAKS:
-      return LandName.GOLDEN_PLAINS;
-    case LandName.HEARTWOOD_GROVE:
-      return LandName.VERDANT_GLADE;
-    case LandName.CRISTAL_BASIN:
-      return LandName.MISTY_GLADES;
-    case LandName.SHADOW_MIRE:
-      return LandName.BLIGHTED_FEN;
-    default:
-      return LandName.NONE;
-  }
+  return nearbySpecialLands[id] ?? LandName.NONE;
 };
 
 /**
@@ -66,28 +54,24 @@ export const getSpecialLandKinds = (): LandType[] => [
  * Returns all regular (non-special) land types
  * @returns Array of regular land types
  */
-export const getRegularLandKinds = (): LandType[] => {
-  return [
-    LandName.PLAINS,
-    LandName.MOUNTAINS,
-    LandName.GREEN_FOREST,
-    LandName.DARK_FOREST,
-    LandName.HILLS,
-    LandName.SWAMP,
-    LandName.DESERT,
-  ];
-};
+export const getRegularLandKinds = (): LandType[] => [
+  LandName.PLAINS,
+  LandName.MOUNTAINS,
+  LandName.GREEN_FOREST,
+  LandName.DARK_FOREST,
+  LandName.HILLS,
+  LandName.SWAMP,
+  LandName.DESERT,
+];
 
 /**
  * Returns the main special land types (primary special lands)
  * @returns Array of main special land types
  */
-export const getMainSpecialLandKinds = (): LandType[] => {
-  return [
-    LandName.VOLCANO,
-    LandName.SUN_SPIRE_PEAKS,
-    LandName.HEARTWOOD_GROVE,
-    LandName.CRISTAL_BASIN,
-    LandName.SHADOW_MIRE,
-  ];
-};
+export const getMainSpecialLandKinds = (): LandType[] => [
+  LandName.VOLCANO,
+  LandName.SUN_SPIRE_PEAKS,
+  LandName.HEARTWOOD_GROVE,
+  LandName.CRISTAL_BASIN,
+  LandName.SHADOW_MIRE,
+];
